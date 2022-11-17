68b99e61de3379b34fcb7f44652aa526866ba8fc
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-17 14:53:23 -0300

    feat: created static route for uploads

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import * as dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -7,10 +8,12 @@ dotenv.config();
 
 const DB_PATH = `${process.env.DB_PATH}`;
 const PORT = process.env.PORT;
+const UPLOAD_PATH = path.resolve(__dirname, "..", "uploads");
 
 mongoose.connect(DB_PATH)
 	.then(() => {
 		const app = express();
+		app.use("/uploads", express.static(UPLOAD_PATH));
 		app.use(express.json());
 		app.use(router);
 		app.listen(PORT, () => console.log("Running"));
