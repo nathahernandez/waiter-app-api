b1d003c19583a0bc8b44d68d0f67d1fdf4dec8d4
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-17 01:35:56 -0300

    feat: product controller rules added (get and post)

--- src/app/controllers/productController/index.ts
+++ src/app/controllers/productController/index.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
+import { Product } from "../../models/schemas/Product/Product";
 
 export const createProduct = async (req: Request, res: Response) => {
-	res.send("ok");
+	const {
+		name,
+		description,
+		price,
+		ingredients,
+		category
+	} = req.body;
+	const imagePath = req.file?.filename;
+
+	await Product.create({
+		name,
+		description,
+		price: Number(price),
+		ingredients: JSON.parse(ingredients),
+		category,
+		imagePath
+	})
+		.then((prod) => res.status(201).send(prod))
+		.catch((err) => res.status(501).send(err));
 };
 
 export const listProducts = async (req: Request, res: Response) => {
-	res.send("ok");
+	await Product.find()
+		.then((list) => res.status(201).send(list))
+		.catch((err) => res.status(501).send(err));
 };
