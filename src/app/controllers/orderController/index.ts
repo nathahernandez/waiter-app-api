adb294c1dfd1325263bb0fc80d9c24b2c104ee86
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-17 01:41:03 -0300

    feat: added multer upload in router and upload folder, minor fixes on products routes

--- src/app/controllers/orderController/index.ts
+++ src/app/controllers/orderController/index.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
+import { Order } from "../../models/schemas/Order/Order";
 
 export const createOrder = async (req: Request, res: Response) => {
-	res.send("ok");
+	const {
+		products,
+		table
+	} = req.body;
+
+	await Order.create({
+		products,
+		table
+	});
+
+	res.status(201).send("Order created.");
 };
+
 export const listOrders = async (req: Request, res: Response) => {
-	res.send("ok");
+	const orders = await Order.find();
+
+	res.status(200).json(orders);
 };
--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,32 @@
+import path from "node:path";
 import { Router } from "express";
+import multer from "multer";
 import { createCategory, listCategories } from "./app/controllers/categoryController";
 import { createOrder, listOrders } from "./app/controllers/orderController";
 import { createProduct, listProducts } from "./app/controllers/productController";
 
 export const router = Router();
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination(req, file, callback){
+			callback(null, path.resolve(__dirname, "..", "uploads"));
+		},
+		filename(req, file, callback ) {
+			callback(null, `${Date.now()}-${file.originalname}`);
+		}
+	}),
+});
+
 //List/Create Categories
 router.get("/categories", listCategories);
 router.post("/categories", createCategory);
 //List/Create Products
-router.get("/products", listOrders);
-router.post("/products", createOrder);
-//List/Create Orders
-router.get("/orders", listProducts);
-router.post("/orders", createProduct);
+router.get("/products", listProducts);
+router.post("/products", upload.single("image") ,createProduct);
+// //List/Create Orders
+// router.get("/orders", listOrders);
+// router.post("/orders", createOrder);
 // List Products By Category
 router.get("/category/:categoryId/products", (req, res) => {
 	res.send("OK");
