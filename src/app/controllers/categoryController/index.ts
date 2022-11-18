b328326d646143268176436ab7713233e24dbac8
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-17 23:46:18 -0300

    chore: improvement on code

--- src/app/controllers/categoryController/index.ts
+++ src/app/controllers/categoryController/index.ts
@@ -5,16 +5,15 @@ import { Product } from "../../models/schemas/Product/Product";
 export const createCategory = async (req: Request, res: Response) => {
 	const { icon, name } = req.body;
 
-	await Category.create({
-		icon,
-		name
-	})
+	await Category
+		.create({ icon, name })
 		.then(category => res.status(201).json(category))
 		.catch(err => res.status(501).json(err));
 };
 
 export const listCategories = async (req: Request, res: Response) => {
-	await Category.find()
+	await Category
+		.find()
 		.then(categories => res.status(200).json(categories))
 		.catch(err => res.status(404).json(err));
 };
@@ -22,7 +21,10 @@ export const listCategories = async (req: Request, res: Response) => {
 export const listByCategory = async (req: Request, res: Response) => {
 	const { categoryId } = req.params;
 
-	await Product.find().where("category").equals(categoryId)
+	await Product
+		.find()
+		.where("category")
+		.equals(categoryId)
 		.then(products => res.status(200).json(products))
 		.catch(err => res.status(404).json(err));
 };
