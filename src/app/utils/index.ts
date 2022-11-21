ad3251bdcfc0513ac39bba7b3fc34ddc2997a352
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-21 02:57:53 -0300

    fix: fixed static uploads

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,12 +1,12 @@
 import path from "node:path";
 import multer from "multer";
 
-export const UPLOAD_PATH = path.resolve(__dirname, "..", "uploads");
+export const UPLOAD_PATH_RESOLVE = path.resolve(__dirname, "..", "uploads");
 
 export const upload = multer({
 	storage: multer.diskStorage({
 		destination(req, file, callback){
-			callback(null, UPLOAD_PATH);
+			callback(null, UPLOAD_PATH_RESOLVE);
 		},
 		filename(req, file, callback ) {
 			callback(null, `${Date.now()}-${file.originalname}`);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { router } from "./router";
-import { UPLOAD_PATH } from "./app/utils";
 
 dotenv.config();
 const DB_PATH = `${process.env.DB_PATH}`;
@@ -11,8 +11,8 @@ mongoose.connect(DB_PATH)
 	.then(() => {
 		const PORT = process.env.PORT;
 		const app = express();
-
-		app.use("/uploads", express.static(UPLOAD_PATH));
+		app.use(cors());
+		app.use("/uploads", express.static("uploads"));
 		app.use(express.json());
 		app.use(router);
 		app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT} 🚝`));
